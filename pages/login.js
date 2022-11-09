f6446c7716ffb45002c1ec2c882183b84d2cbf8b
Cian O'Donnell <G00358872@atu.ie>
2022-11-09 18:42:03 +0000

    add login files

---
+++ pages/login.js
@@ -0,0 +1,34 @@
+
+import styles from '../styles/login.module.css'
+
+export default login;
+
+function login() {
+  return (
+    <>
+
+      <main className={form-signin}>
+        <formy>
+          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+
+          <div className={styles.checkbox}>
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </formy>
+      </main>
+
+    </>
+  )
+}
