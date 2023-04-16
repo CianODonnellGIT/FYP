8eb16bfc4997af296f96d79db62c839c0dfe1f2c
CianODonnellGIT <115069068+CianODonnellGIT@users.noreply.github.com>
2023-04-17 00:16:36 +0100

    Delete controller directory

--- controller/Employee.js
+++
@@ -1,7 +0,0 @@
-import pool, { executeQuery } from '../config/Db';
-const getAllEmployees = async (req,res) => {
-    let employeeData = await executeQuery('select * from employee',[]);
-    res.send(employeeData);
-};
-
-export {getAllEmployees};
