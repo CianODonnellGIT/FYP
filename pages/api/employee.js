a3e9b9421356ead64382d0a9e8646d33e1b004d3
CianODonnellGIT <115069068+CianODonnellGIT@users.noreply.github.com>
2023-04-17 00:27:17 +0100

    Delete employee.js

--- pages/api/employee.js
+++
@@ -1,9 +0,0 @@
-import nc from "next-connect";
-import { getAllEmployees } from "../../controller/Employee"
-
-const handler = nc();
- // .use(someMiddleware())
-  handler.get(getAllEmployees);
-  
-
-export default handler;
