d01cc8a4957780cc519006f64a3649ad7d1578c9
CianODonnellGIT <115069068+CianODonnellGIT@users.noreply.github.com>
2023-05-05 14:26:41 +0100

    Updated with Auth0 Authentication

---
+++ pages/api/auth/[...auth0].js
@@ -0,0 +1,7 @@
+/*
+This code was inspired by the following reference
+    GitHub, "auth0/nextjs-auth0: Next.js SDK for signing in with Auth0" [Online]. Available: https://github.com/auth0/nextjs-auth0.
+*/
+import { handleAuth } from '@auth0/nextjs-auth0';
+
+export default handleAuth();
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from 'next/head'
-import {CiTrash} from 'react-icons/ci'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
-import Crud from '../components/comp/Crud'
 import Login from '../components/comp/Login'
 
-import { useState, useRef, useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   
