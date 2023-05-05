8bc17a34905ce8054669e7fd42c5a53a1f3e8f99
CianODonnellGIT <115069068+CianODonnellGIT@users.noreply.github.com>
2023-05-05 14:29:47 +0100

    updated with reference

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
+/*
+This code was inspired by the following reference
+    GitHub, "olbega/nextjs-crud-mysql" [Online]. Available: https://github.com/oelbaga/nextjs-crud-mysql.
+    GitHub, "auth0/nextjs-auth0: Next.js SDK for signing in with Auth0" [Online]. Available: https://github.com/auth0/nextjs-auth0.
+*/
 import mysql from "mysql2/promise";
 
 export async function query({query, value = []}){
 
     const mySQLconnect = await mysql.createConnection({
-        host: "fyp-database.cauvst9l0ldn.eu-west-1.rds.amazonaws.com",
-        user: "admin",
-        password:"firstData&",
+        host: 'fyp-database.cauvst9l0ldn.eu-west-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'firstData&',
         port: 3306,
-        database: "fypdb"
+        database: 'fypdb'
     });
 
     try{
@@ -18,3 +23,4 @@ export async function query({query, value = []}){
         throw Error(error.message);
     }
 }
+
